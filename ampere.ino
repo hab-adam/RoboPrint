2ed9d1d3c8c2e9b364111d44f73d491b6848f551
adam <adamhabachirov@gmail.com>
2021-07-15 16:48:24 +0300

    added repo

---
+++ ampere.ino
@@ -0,0 +1,18 @@
+int ampere;
+#define KEY 7
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  pinMode(KEY, OUTPUT);
+  Serial.println("START\n");
+}
+
+void loop() {
+  ampere = analogRead(1);
+  Serial.println(ampere);
+  if (ampere > 550)
+    digitalWrite(KEY, HIGH);
+  else
+    digitalWrite(KEY, LOW);
+}
